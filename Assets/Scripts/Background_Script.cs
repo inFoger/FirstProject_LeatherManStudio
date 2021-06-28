a1f6477337cad9b363d73d8ed95d5ab00ca3f85f
inFoger <The-infoger@yandex.ru>
2021-06-28 19:59:16 +0600

    [+]Создал скрипт для движения заднего фона

---
+++ Assets/Scripts/Background_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Script : MonoBehaviour
+{
+    public Camera _camera;
+    private Transform _transform;
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    
+    void Update()
+    {
+        _transform.position = new Vector3(_transform.position.x, _camera.transform.position.y + 121, _transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
