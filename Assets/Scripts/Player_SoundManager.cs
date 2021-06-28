3faf4b5caabcfdc8b1fe8a5ee58c1e25376173f0
inFoger <The-infoger@yandex.ru>
2021-06-28 18:27:33 +0600

    [+] Добавлены звуки ударов, получения урона, шагов и прыжка
    [+]Добавил задний фон
    [*]Изменения в основной сцене. Пофиксил отображение спрайтов.

---
+++ Assets/Scripts/Player_SoundManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_SoundManager : MonoBehaviour
+{
+    public AudioClip steps1;
+    public AudioClip steps2;
+    public AudioClip landing;
+    public AudioClip attack1;
+    public AudioClip attack2;
+    public AudioClip attack3;
+    public AudioClip hurt;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Steps1()
+    {
+        _audioSource.PlayOneShot(steps1);
+    }
+
+    void Steps2()
+    {
+        _audioSource.PlayOneShot(steps2);
+    }
+
+    void Landing()
+    {
+        _audioSource.PlayOneShot(landing);
+    }
+
+    void Attack1()
+    {
+        _audioSource.PlayOneShot(attack1);
+    }
+
+    void Attack2()
+    {
+        _audioSource.PlayOneShot(attack2);
+    }
+
+    void Attack3()
+    {
+        _audioSource.PlayOneShot(attack3);
+    }
+
+    void Hurt()
+    {
+        _audioSource.PlayOneShot(hurt);
+    }
+
+}
